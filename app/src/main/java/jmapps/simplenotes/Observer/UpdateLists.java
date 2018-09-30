29b5bf6c9a24cf5e656609c7647f486cf0383546
JMApps <jmappsmuslim@gmail.com>
2018-09-30 23:03:10 +0300

    Added observer

---
+++ app/src/main/java/jmapps/simplenotes/Observer/UpdateLists.java
@@ -0,0 +1,26 @@
+package jmapps.simplenotes.Observer;
+
+import java.util.Observable;
+
+public class UpdateLists extends Observable {
+
+    private boolean updateAdapterLists;
+
+    private static UpdateLists INSTANCE;
+
+    public static UpdateLists getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new UpdateLists();
+        }
+        return INSTANCE;
+    }
+
+    public boolean isUpdateAdapterLists() {
+        return updateAdapterLists;
+    }
+
+    public void setUpdateAdapterLists(boolean updateAdapterLists) {
+        this.updateAdapterLists = updateAdapterLists;
+        setChanged();
+    }
+}
